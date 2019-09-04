193e2ccdfa1dd11852f7b790be2d857ce02bb77d
AYUSH UJJWAL <42496674+ayush1612@users.noreply.github.com>
2019-09-04 12:27:43 +0530

    Create assignment1.py

---
+++ SL-lab/Lab-1/assignment1.py
@@ -0,0 +1,15 @@
+list = [1,'2',"three"]
+
+print(len(list))
+
+list.append([4,'5',"six"])
+print(list)
+
+print(list[1:2])
+
+list[1]="apple"
+print(list)
+
+list2 = [7,"8","nine"]
+
+print(list+list2)
