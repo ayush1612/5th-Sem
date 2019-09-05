f47585462d19287eed4e8180c6db34bce3ce964a
AYUSH UJJWAL <42496674+ayush1612@users.noreply.github.com>
2019-09-05 22:29:56 +0530

    Create prg2.py

---
+++ SL-lab/Lab-1/assignment/prg2.py
@@ -0,0 +1,14 @@
+# creating a tuple with numbers
+
+tup = (1,2,3)
+
+# print one item of the tuple
+print("First Item of the Tuple : ",tup[0])
+
+# Convert this tuple to a list
+ls = []
+
+for i in tup:
+    ls.append(i)
+
+print(ls)
