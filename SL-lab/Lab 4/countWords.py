a20286dc1d842ddf6f2e32c1e34fefb404d35023
AYUSH UJJWAL <42496674+ayush1612@users.noreply.github.com>
2019-09-13 10:02:33 +0530

    Create countWords.py

---
+++ SL-lab/Lab 4/countWords.py
@@ -0,0 +1,14 @@
+#Write the python program to count the frequency of words in a given file
+from collections import Counter
+def word_count(fname):
+	with open(fname) as f:
+		return Counter(f.read().split())
+		
+print("Number of words in the file:",word_count("test.txt"))
+
+# ==============================================================
+# Explanation
+# line 2 : imported counter from the library 'Collections'
+# line 3 : made a function named 'word_count'
+# line 4 : opened a file using fname and stored in a variable 'f'
+# line 5 : return the number of words in the file having that string
