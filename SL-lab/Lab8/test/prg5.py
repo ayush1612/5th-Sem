39bcc8866047bea4a6fc63acc14198b37de07afe
AYUSH UJJWAL <42496674+ayush1612@users.noreply.github.com>
2019-10-09 12:24:19 +0530

    Update prg5.py

--- SL-lab/Lab8/test/prg5.py
+++ SL-lab/Lab8/test/prg5.py
@@ -4,4 +4,11 @@ def OddRange(num1,num2):
         if i%2 != 0:
             odd.append(i)
     return odd
-print(OddRange(1,7))
+
+# taking the input and converting it to integer 
+# input() takes the input
+# int() converts it to integer
+
+num1 = int(input())
+num2 = int(input())
+print(OddRange(num1,num2))
