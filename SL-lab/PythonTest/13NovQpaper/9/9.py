4788d03d9e0bc6d7347a7d94007014d35d6150b4
AYUSH UJJWAL <42496674+ayush1612@users.noreply.github.com>
2019-11-27 00:17:07 +0530

    Added 9

---
+++ SL-lab/PythonTest/13NovQpaper/9/9.py
@@ -0,0 +1,33 @@
+if __name__ == "__main__":          # this line is not necessary in this program (it depicts the main function)
+# i) Create a dictionary 
+    atomDict = {
+        'Na' : 'Sodium',
+        'K' : 'Potassium',
+        'Mn' : 'Manganese',
+        'Mg' : 'Magnesium',
+        'Li' : 'Lithium'
+    }
+
+# ii) Adding unique and duplicate elements in the dictionary
+    print('Dictinary before :',atomDict)
+
+    atomDict['He'] = 'Helium'
+    print('Dictionary after adding a unique element :',atomDict)
+    # a new key value pair will be added as 'He' : 'Helium'
+
+    atomDict['Na'] = 'SODIUM'
+    print('Dictionary after adding a duplicate value :',atomDict)
+    # 'Na' value will change from 'Sodium' to 'SODIUM'
+
+# iii) Number of elements in the dictionary
+
+    print('Number of Atomic Elements in the dictionary are : ',len(atomDict.items()))
+
+# iv) Ask user to enter an element and search in the dictionary
+
+    searchElement = input('Enter an element to search in the dictionary:\n')
+
+    if searchElement not in atomDict.keys():        #dict.keys() returns the list of keys in the doctionary
+        print('Element not in the dictionary')
+    else:
+        print('Name of the element ',searchElement,' is ',atomDict[searchElement])  
---
+++ SL-lab/PythonTest/13NovQpaper/9/9rewrite/Atomic.py
@@ -0,0 +1,4 @@
+from file import AtomicDictionary
+# Now I can use the function AtomicDictionary()
+
+AtomicDictionary()
